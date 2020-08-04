60c410c1eacfc497e0c7c76cf89ef401fcfa42c6
wayne.han <wayne.han@picovr.com>
2020-08-04 20:23:07 +0800

    build star rating widget

---
+++ lib/douban/main.dart
@@ -0,0 +1,121 @@
+import 'package:first_flutter/douban/utils.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(DouBanApp());
+
+class DouBanApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: MyHomepage(),
+    );
+  }
+}
+
+class MyHomepage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("This is title"),
+      ),
+      body: MyContentBody(),
+    );
+  }
+}
+
+class MyContentBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: StarRating(
+        rating: 7,
+      ),
+    );
+  }
+}
+
+class StarRating extends StatefulWidget {
+  final double rating;
+  final double maxRating;
+  final int count;
+  final double size;
+  final Widget unselectedWidget;
+  final Widget selectedWidget;
+  final Color unSelectColor;
+  final Color selectColor;
+
+  StarRating(
+      {double rating,
+      this.maxRating = 10,
+      this.count = 5,
+      this.size = 50,
+      Widget unselectedWidget,
+      Widget selectedWidget,
+      this.unSelectColor = Colors.grey,
+      this.selectColor = Colors.redAccent})
+      : unselectedWidget = unselectedWidget ??
+            Icon(
+              Icons.star,
+              size: size,
+              color: unSelectColor,
+            ),
+        selectedWidget =
+            selectedWidget ?? Icon(Icons.star, size: size, color: selectColor),
+        rating = rating > maxRating ? maxRating : rating;
+
+  @override
+  _StarRatingState createState() => _StarRatingState();
+}
+
+class _StarRatingState extends State<StarRating> {
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: buildUnSelectWidget(),
+        ),
+        Row(
+          mainAxisSize: MainAxisSize.min,
+          children: buildSelectWidget(),
+        ),
+      ],
+    );
+  }
+
+  /// 创建未选中的Widget
+  List<Widget> buildUnSelectWidget() {
+    return List.generate(
+      widget.count,
+      (index) => widget.unselectedWidget,
+    );
+  }
+
+  /// 创建选中的Widget
+  List<Widget> buildSelectWidget() {
+    List<Widget> widgets = [];
+    // 1.计算Star个数和剩余比例等
+    double oneValue = widget.maxRating / widget.count;
+    int entireCount = (widget.rating / oneValue).floor();
+    double leftValue = widget.rating - entireCount * oneValue;
+    double leftRatio = leftValue / oneValue;
+    print("计算：$oneValue, $entireCount, $leftValue, $leftRatio");
+    // 2.集合中加入[填充满的widget]
+    for (int i = 0; i < entireCount; i++) {
+      widgets.add(widget.selectedWidget);
+    }
+    // 3.集合中加入[剩余部分的widget]
+    if (leftValue > 0) {
+      widgets.add(
+        ClipRect(
+          child: widget.selectedWidget,
+          clipper: MyRectClipper(leftRatio * widget.size),
+        ),
+      );
+    }
+    return widgets;
+  }
+}
---
+++ lib/douban/utils.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+/// 自定义裁剪类
+class MyRectClipper extends CustomClipper<Rect> {
+  final double width;
+
+  MyRectClipper(this.width);
+
+  @override
+  Rect getClip(Size size) {
+    return Rect.fromLTRB(0, 0, width, size.height);
+  }
+
+  @override
+  bool shouldReclip(MyRectClipper oldClipper) {
+    return width != oldClipper.width;
+  }
+}
